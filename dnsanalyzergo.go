221d3c7de7adccf1d39a96f53b717b739619fda3
Jay Looney <jay.m.looney@gmail.com>
2017-02-14 05:58:08 -0600

    Began Refactoring Standalone Binary into Library
    Moved all the core code powering the binary out of the main function
    and into an analyzeDns function. Call `analyzeDns` with the parameters grabbed by
    `flag.Parse()`. Instead of repetitive looping for min/avg/max/stddev/
    calculations in the function body, I implemented Min(), Avg(), Max(),
    and Std() methods on the DurationSlice type. This made some of the code
    look prettier but it might turn out to be a bad idea.

--- dnsanalyzergo.go
+++ dnsanalyzergo.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"math"
-	"sort"
-	"time"
-
-	"github.com/miekg/dns"
-)
-
-type DurationSlice []time.Duration
-
-func (p DurationSlice) Len() int           { return len(p) }
-func (p DurationSlice) Less(i, j int) bool { return int64(p[i]) < int64(p[j]) }
-func (p DurationSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+import "flag"
 
 func main() {
 	samplesPtr := flag.Int("samples", 100, "How many DNS Requests you want to make")
@@ -24,42 +9,5 @@ func main() {
 
 	flag.Parse()
 
-	m := new(dns.Msg)
-	m.Id = dns.Id()
-	m.RecursionDesired = true
-	m.Question = make([]dns.Question, 1)
-	m.Question[0] = dns.Question{dns.Fqdn(*domainPtr), dns.TypeA, dns.ClassINET}
-
-	c := new(dns.Client)
-
-	fmt.Printf("QUERY %v (@%v):  %v data bytes\n", *domainPtr, *serverPtr, m.Len())
-
-	rtts := make(DurationSlice, *samplesPtr, *samplesPtr)
-	for i := 0; i < *samplesPtr; i++ {
-		in, rtt, err := c.Exchange(m, *serverPtr+":53")
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		rtts[i] = rtt
-		fmt.Printf("%v bytes from %v: ttl=%v time=%v\n", in.Len(), *serverPtr, time.Second*6, rtt)
-	}
-	sort.Sort(rtts)
-
-	var min, max, avg, avgsqdif, stdev time.Duration
-	min = time.Duration(rtts[0])
-	max = time.Duration(rtts[len(rtts)-1])
-	avg = (min + max) / 2
-
-	sqdifs := make(DurationSlice, *samplesPtr, *samplesPtr)
-	for j := 0; j < *samplesPtr; j++ {
-		sqdif := rtts[j] - avg
-		sqdifs[j] = sqdif * sqdif
-	}
-	sort.Sort(sqdifs)
-
-	avgsqdif = (sqdifs[0] + sqdifs[len(sqdifs)-1]) / 2
-	stdev = time.Duration(math.Sqrt(float64(avgsqdif)))
-
-	fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n", min, avg, max, stdev)
+	analyzeDns(*serverPtr, *domainPtr, *samplesPtr)
 }
---
+++ lib.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"sort"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+type DurationSlice []time.Duration
+
+// NOTE: This implements the sortable interface
+func (p DurationSlice) Len() int           { return len(p) }
+func (p DurationSlice) Less(i, j int) bool { return int64(p[i]) < int64(p[j]) }
+func (p DurationSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// NOTE: Wasteful Convenience Functions
+func (p DurationSlice) Min() time.Duration {
+	sort.Sort(p)
+	return p[0]
+}
+func (p DurationSlice) Max() time.Duration {
+	sort.Sort(p)
+	return p[p.Len()-1]
+}
+func (p DurationSlice) Avg() time.Duration {
+	var avg int64
+	for i := 0; i < p.Len(); i++ {
+		avg += int64(p[i])
+	}
+	return time.Duration(avg / int64(p.Len()))
+}
+func (p DurationSlice) Std() time.Duration {
+	sqdifs := make(DurationSlice, p.Len(), p.Len())
+	avg := p.Avg()
+	var avgsqdif int64
+	for i := 0; i < p.Len(); i++ {
+		sqdif := p[i] - avg
+		sqdifs[i] = sqdif * sqdif
+		avgsqdif += int64(sqdifs[i])
+	}
+	avgsqdif /= int64(sqdifs.Len())
+	return time.Duration(math.Sqrt(float64(avgsqdif)))
+}
+
+// TODO(zenware): make this output to a writer
+func analyzeDns(server, hostname string, samples int) {
+	m := new(dns.Msg)
+	m.Id = dns.Id()
+	m.RecursionDesired = true
+	m.Question = make([]dns.Question, 1)
+	m.Question[0] = dns.Question{dns.Fqdn(hostname), dns.TypeA, dns.ClassINET}
+
+	c := new(dns.Client)
+
+	fmt.Printf("QUERY %v (@%v):  %v data bytes\n", hostname, server, m.Len())
+
+	rtts := make(DurationSlice, samples, samples)
+	for i := 0; i < samples; i++ {
+		in, rtt, err := c.Exchange(m, server+":53")
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		rtts[i] = rtt
+		fmt.Printf("%v bytes from %v: ttl=%v time=%v\n", in.Len(), server, time.Second*6, rtt)
+	}
+
+	// NOTE: Potentially Eating Performance for Pretties
+	var min, max, avg, stddev time.Duration
+	min = rtts.Min()
+	max = rtts.Max()
+	avg = rtts.Avg()
+	stddev = rtts.Std()
+
+	fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n", min, avg, max, stddev)
+}
